b4f156994837db145641cf355368d7d6a2248882
Yann Berthou <firetonton@gmail.com>
2018-11-25 17:09:31 +0100

    Dockerize #1

--- run.rb
+++ run.rb
@@ -1,8 +1,32 @@
 #!/usr/bin/env ruby
 require './prism_rss'
 
+NB_LOOPS = if ENV['NB_LOOPS'].to_i > 0
+             ENV['NB_LOOPS'].to_i
+           elsif ENV['NB_LOOPS'].to_i < 0
+             Float::INFINITY
+           else
+             1
+           end
+# Sleep time (in minutes)
+MIN_SLEEP_TIME = 1
+DEF_SLEEP_TIME = 10
+SLEEP_TIME = if ENV['SLEEP_TIME'].nil?
+               DEF_SLEEP_TIME
+             else
+               [ENV['SLEEP_TIME'].to_i, MIN_SLEEP_TIME].max
+             end
+
 begin
-  PrismRSS.new.run
+  (1..NB_LOOPS).each do |i|
+    puts "#{Time.now} run #{i}"
+    PrismRSS.new.run
+    if i < NB_LOOPS
+      puts "#{Time.now} Sleep #{SLEEP_TIME} minutes"
+      sleep(SLEEP_TIME * 60)
+    end
+  end
+  puts "#{Time.now} done"
 rescue => e
   puts(e.message)
   puts(*e.backtrace)
